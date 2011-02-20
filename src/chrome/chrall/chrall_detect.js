44764d330a17b06e407ad940f14d9d412ef6d6af
Canop <cano.petrole@gmail.com>
2011-02-20 19:34:37 +0100

    Correction du site web : ne prétendons pas qu'il y a une mise à jour automatique, ça ne marche pas...

--- src/chrome/chrall/chrall_detect.js
+++ src/chrome/chrall/chrall_detect.js
@@ -1,5 +1,5 @@
 
 var paragraph = $("p#browserInfos");
 if (paragraph) {
-	paragraph.html("Chrall est déjà installé dans ce navigateur et fonctionne. Vous pouvez cliquer sur le lien ci-dessus si vous le souhaitez (pour réinstaller ?) mais cela est probablement inutile.");
+	paragraph.html("Chrall est déjà installé dans ce navigateur et fonctionne. Vous pouvez cliquer sur le lien ci-dessus si vous souhaitez être sûr d'avoir la dernière version (une mise à jour automatique est sensée être en place mais ça n'a pas l'air de marcher).");
 }
