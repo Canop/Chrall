0a3ebdb5143cc3c9ea220f073583fe3b3ec83580
zerod <zerod@free.fr>
2011-02-24 22:58:28 +0100

    phase initiale du traitement des mouches, on pourra eventuellement le rendre plus esthetique

--- src/chrome/chrall/chrall_flies.js
+++ src/chrome/chrall/chrall_flies.js
@@ -12,33 +12,49 @@ function Chrall_analyseFlies() {
 		var aFly = new Fly(aFlyType);
 		player.addFly(aFly);
 	}
-	alert(player.flies.length+" mouches");
 	
 }
 
 
 function Chrall_analyseAndReformatFlies() {
-	//> on vire la bannière "Mounty Hall la terre des trolls" 
-	
-	$($("tr")[0]).append("<tr>TOOOOOOOTOOOO</tr>");
+
 
 	//> on vire le titre
 	$($("table")[2]).remove();
 	
 	// analyse du tableau de mouches
-	//Chrall_analyseFlies($($("table.mh_tdborder")[0]).text());
 	Chrall_analyseFlies();
 	
 	// calcul des totaux 
-	//	TOUR : -140 min | Armure : +10 | ATT : +8 | REG : +7 | DEG : +9 | Vue : +5 | ESQ : +6
 	var att=0,esq=0,deg=0,vue=0,pv=0,arm=0,reg=0,cyc=0;
-	
+	var aFlyType;
 	for(var i=0;i<player.flies.length; i++){
-		aFly=player.flies[i];
-		if()
+		aFlyType=player.flies[i].type;
+		
+		if(aFlyType=="Rivatant"){
+			cyc+=20;
+		} else if(aFlyType=="Xidant"){
+			arm++;
+		} else if(aFlyType=="Crobate"){
+			esq++;
+		} else if(aFlyType=="Miel"){
+			reg++;
+		} else if(aFlyType=="Nabolisants"){
+			deg++;
+		} else if(aFlyType=="Vertie"){
+			att++;
+		}  else if(aFlyType=="Lunettes"){	
+			vue++;
+		} else if(aFlyType=="Telaite"){
+			pv+=5;
+		}
+		  
 	}
 	
+	var totaux="att +"+att+" / esq +"+esq+" / deg +"+deg+" / arm +"+arm+" / Tour +"+cyc+" min / pv +"+pv+" / vue +"+vue+" / reg +"+reg;
+	
+	var html="<table width=98% cellspacing=0 border=0 cellpadding=2 align=left><tbody><tr><td align=left><b>"+totaux+"</b></td></tr></tbody></table>";
 	// insertion des bonus mouchesque.
-	//$("tr.mh_tdtitre").append("<tr class=mh_tdpage>Total des bonus de mouches : </tr>");
+	$('form[name="ActionForm"]').parent().prepend(html);
 
 }
