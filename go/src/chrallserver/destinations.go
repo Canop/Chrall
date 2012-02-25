a3acb70e2ab2840e09b78f6875baafa035c25629
Frederic Donckels <frederic.donckels+github-chrall@gmail.com>
2012-02-25 21:07:35 +0100

    Reset sur jsongethandler.go modifié par erreur (manque de maitrise de git)

--- go/src/chrallserver/destinations.go
+++ go/src/chrallserver/destinations.go
@@ -1,23 +1,22 @@
 package main
+
 /*
 gère les destinations pour les Tps et les gowaps. Le concept de destination est juste une vue de l'esprit,
 une réduction-agrégation de plusieurs sources de données.
 */
 import (
 	"fmt"
-	"mysql"
 	"strings"
 )
 
 // une destination
 type Destination struct {
-	Auteur    int
-	X         int
-	Y         int
-	Z         int
-	Desc      string
+	Auteur int
+	X      int
+	Y      int
+	Z      int
+	Desc   string
 }
-
 
 func JoinIds(ids []int, sep string) string {
 	if 0 == len(ids) {
@@ -25,14 +24,14 @@ func JoinIds(ids []int, sep string) string {
 	}
 	stringIds := make([]string, len(ids))
 	for i, val := range ids {
-		stringIds[i] = string(val)  // string.join ne prend pas de slice d'objets quelconques comme paramètres
+		stringIds[i] = string(val) // string.join ne prend pas de slice d'objets quelconques comme paramètres
 	}
 
 	fmt.Println("Joined Ids: ", strings.Join(stringIds, ","))
 	return strings.Join(stringIds, ",")
 }
 
-func (store *MysqlStore) GetDestinations(db *mysql.Client, amis []int) (destinations []*Destination) {
+func (store *MysqlStore) GetDestinations(db *sql.DB, amis []int) (destinations []*Destination) {
 	sql := "select id, x, y, z  from compte where id in (?) "
 	statement, err := db.Prepare(sql)
 	idList := strings.Join(amis, ",")
