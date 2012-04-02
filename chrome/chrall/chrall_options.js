57e6a5b984ab01b30eabae837320cd3320392da9
Frederic Donckels <frederic.donckels+github-chrall@gmail.com>
2012-04-02 19:36:24 +0200

    Sections plus esthétiques dans les options

--- chrome/chrall/chrall_options.js
+++ chrome/chrall/chrall_options.js
@@ -17,30 +17,43 @@
 	<div class=tab_container><br><br>\
 	<div id=tabStandard class=tab_content></div>\
 	<div id=tabChrall class=tab_content>\
-	<h3>Qu'est-ce que Chrall?</h3>\
-		<p class='informational-text'>Chrall est une extension pour Chrome visant à enrichir l'interface du jeu.</p>\
-		<p class='informational-text'>Si vous souhaitez retrouver l'interface standard de Mounty Hall, vous pouvez la désactiver via le menu <i>Outils/Extensions</i> de Chrome.</p>\
-		<p class='informational-text'>Pour plus d'informations sur le projet Chrall : <a target=newWin href=http://canop.org/chrall>canop.org/chrall</a>.</p>\
-	<h3>Que fait Chrall exactement?</h3>\
-		<p class='informational-text'>Chrall modifie les pages que vous recevez depuis *.mountyhall.com et quelques adresses alternatives du jeu (Chrall ne voit ni ne change les autres pages).</p>\
-		<p class='informational-text'>Chrall affiche, sur la vue, des estimations des caractéristiques des monstres, en exploitant son bestiaire (lien plus bas). De même pour les trolls en exploitant des informations publiques (par exemple la liste des kills).</p>\
-		<p class='informational-text'>Chrall transmet les CDM que vous effectuez à ce bestiaire, afin de l'enrichir (si vous l'acceptez).</p>\
-	<h3>Compte Chrall</h3>\
-		<p>Etat du compte Chrall : <b><span id=com_status_message></span></b></p>\
-		<p class='informational-text'>Un compte Chrall vous permet de partager des informations avec d'autres joueurs. Il n'est nullement nécessaire d'avoir un compte sur Chrall pour exploiter l'extension. Ce compte n'a d'intérêt que si vous chassez avec d'autres.</p>\
-		<p>Afin d'authentifier les requêtes provenant au serveur Chrall, votre <a href='http://sp.mountyhall.com/md5.php' target=newTab>mot de passe restreint</a> est nécessaire :</p>\
-		<p><input type=password id=ch_mdp_restreint value=''></p>\
-		<a class=gogo id=changeMdp>Modifier le mot de passe</a> &nbsp; <a id=activationButton class=gogo />\
-		<p class='informational-text'>Notez que vous ne transmettez pas d'informations confidentielles tant que vous n'activez pas le compte.</p>\
-		<p class='informational-text'>Si votre compte est actif, l'extension communique par défaut les données partagées avec le serveur canop.org mais vous pouvez spécifier un autre serveur si vous lui accordez confiance : </p>\
-		<p>Serveur Chrall alternatif pour les données partagées : <input size=30 id=input_private_chrall_server></p>\
+		<h3 class='option-section'>Qu'est-ce que Chrall?</h3>\
+		<div class='option-section'>\
+			<p class='informational-text'>Chrall est une extension pour Chrome visant à enrichir l'interface du jeu.</p>\
+			<p class='informational-text'>Si vous souhaitez retrouver l'interface standard de Mounty Hall, vous pouvez la désactiver via le menu <i>Outils/Extensions</i> de Chrome.</p>\
+			<p class='informational-text'>Pour plus d'informations sur le projet Chrall : <a target=newWin href=http://canop.org/chrall>canop.org/chrall</a>.</p>\
+		</div>\
+		<p/>\
+		<h3 class='option-section'>Que fait Chrall exactement?</h3>\
+		<div class='option-section'>\
+			<p class='informational-text'>Chrall modifie les pages que vous recevez depuis *.mountyhall.com et quelques adresses alternatives du jeu (Chrall ne voit ni ne change les autres pages).</p>\
+			<p class='informational-text'>Chrall affiche, sur la vue, des estimations des caractéristiques des monstres, en exploitant son bestiaire (lien plus bas). De même pour les trolls en exploitant des informations publiques (par exemple la liste des kills).</p>\
+			<p class='informational-text'>Chrall transmet les CDM que vous effectuez à ce bestiaire, afin de l'enrichir (si vous l'acceptez).</p>\
+		</div>\
+		<p/>\
+		<h3 class='option-section'>Compte Chrall</h3>\
+		<div class='option-section'>\
+			<p>Etat du compte Chrall : <b><span id=com_status_message></span></b></p>\
+			<p class='informational-text'>Un compte Chrall vous permet de partager des informations avec d'autres joueurs. Il n'est nullement nécessaire d'avoir un compte sur Chrall pour exploiter l'extension. Ce compte n'a d'intérêt que si vous chassez avec d'autres.</p>\
+			<p>Afin d'authentifier les requêtes provenant au serveur Chrall, votre <a href='http://sp.mountyhall.com/md5.php' target=newTab>mot de passe restreint</a> est nécessaire :</p>\
+			<p><input type=password id=ch_mdp_restreint value=''></p>\
+			<a class=gogo id=changeMdp>Modifier le mot de passe</a> &nbsp; <a id=activationButton class=gogo />\
+			<p class='informational-text'>Notez que vous ne transmettez pas d'informations confidentielles tant que vous n'activez pas le compte.</p>\
+			<p class='informational-text'>Si votre compte est actif, l'extension communique par défaut les données partagées avec le serveur canop.org mais vous pouvez spécifier un autre serveur si vous lui accordez confiance : </p>\
+			<p>Serveur Chrall alternatif pour les données partagées : <input size=30 id=input_private_chrall_server></p>\
 		<a href='#' class=gogo id=save_private_chrall_server>sauver</a> (laisser vide pour exploiter le serveur par défaut, n'utilisez un serveur alternatif que si vous lui faites pleine confiance)</p>\
+		</div>\
 	</div>\
 	<div id=tabOptionsChrall class=tab_content>\
-	<h3>Options</h3>\
-		<p class='informational-text'>Modifiez ici le comportement de l'extension</p>\
-	<div id=tabLinks class=tab_content>\
+		<h3 class='option-section'>Vue</h3>\
+		<div class='option-section'>\
+			<p class='informational-text'>Modifiez ici le comportement de l'extension en ce qui concerne la vue 2D.</p>\
+			<div style='display:block'><input id='view-show-distance-in-view' type='checkbox' class='toggle-option'><span class='option-description'>Afficher la distance en PA dans les tables</span></div>\
+			<div style='display:block'><input id='view-display-hit-points-ratio' type='checkbox' class='toggle-option'><span class='option-description'>Afficher la barre de points de vie dans les tables (uniquement pour les partages actifs).</span></div>\
+		</div>\
+		</p>\
 	</div>\
+	<div id=tabLinks class=tab_content>\
 	</div>";
 
 		standardOptionContainer.html(html);
@@ -81,6 +94,13 @@
 			} else {
 				localStorage.removeItem('private_chrall_server');
 			}
+		});
+
+		$(".toggle-option").each(function() {
+			var id = $(this).attr("id");
+			var checked = localStorage[id] == 'yes';
+			this.checked = checked;
+			$(this).change(toggleOption);
 		});
 	}
 
@@ -132,6 +152,11 @@
 			chrall.initCommunications('check_account');
 		}
 		refreshActivation();
+	}
+
+	function toggleOption() {
+		var id = $(this).attr('id');
+		localStorage[id] = this.checked ? "yes": "no";
 	}
 
 })(window.chrall = window.chrall || {});
