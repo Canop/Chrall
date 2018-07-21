e4101bfe14c3fcd740da0b9f1f2333d79e72ce65
Canop <cano.petrole@gmail.com>
2018-07-21 18:38:48 +0200

    fix #chrall set teams often not working
    mutations detected by the mutationobserver weren't complete. Some
    nodes were missing.

--- chrome/chrall/chrall_miaou.js
+++ chrome/chrall/chrall_miaou.js
@@ -5,49 +5,33 @@
 	if (!ùmessages) return;
 
 	var observer = new MutationObserver(function(mutations){
-		for (var i=0; i<mutations.length; i++) {
-			var nodes = mutations[i].addedNodes;
-			if (!nodes || !nodes.length) continue;
-			for (var j=0; j<nodes.length; j++) {
-				if (!/\buser-messages\b/.test(nodes[j].className)) continue;
-				var messsageNodes = nodes[j].childNodes;
-				for (var k=0; k<messsageNodes.length; k++) {
-					if (!/\bmessage\b/.test(messsageNodes[k].className)) continue;
-					var childs = messsageNodes[k].childNodes;
-					for (var l=childs.length; l--;) {
-						var child = childs[l];
-						if (!/\bcontent\b/.test(child.className)) continue;
-						var text = child.innerText; // innerText préserve les \n contrairement à textContent...
-						if (/^.?\s?#chrall ./.test(text)) {
-							var lines = text.split('\n').filter(Boolean);
-							handleChrallCommand(
-								lines[0].match(/^.?\s?#chrall (.*)/)[1].trim(), // la commande
-								lines.slice(1), // les lignes du message après la commande
-								nodes[j], // l'élement .message
-								child // l'élément .content
-							);
-						}
-						break;
-					}
-				}
+		$("#messages .message .content").each(function(){
+			var text = this.innerText; // innerText préserve les \n contrairement à textContent...
+			if (/^.?\s?#chrall ./.test(text)) {
+				var lines = text.split('\n').filter(Boolean);
+				handleChrallCommand(
+					lines[0].match(/^.?\s?#chrall (.*)/)[1].trim(), // la commande
+					lines.slice(1), // les lignes du message après la commande
+					this // l'élément .content
+				);
 			}
-		}
+		});
 	});
 	observer.observe(ùmessages, {childList:true});
 
-	function handleChrallCommand(command, lines, ùm, ùc){
+	function handleChrallCommand(command, lines, ùc){
 		console.log("Chrall command", command);
 		switch (command) {
 		case "set teams":
-			return handleSetTeams(lines, ùm, ùc);
+			return handleSetTeams(lines, ùc);
 		case "set cells":
-			return handleSetCells(lines, ùm, ùc);
+			return handleSetCells(lines, ùc);
 		default:
 			console.log("Commande Chrall incomprise");
 		}
 	}
 
-	function handleSetTeams(lines, ùm, ùc){
+	function handleSetTeams(lines, ùc){
 		$('<button>').text("Définir les équipes dans Chrall").prependTo(ùc).click(function(){
 			var $ok = $('<div><i>Trolls ajoutés</i></div>').hide();
 			$(this).replaceWith($ok);
@@ -75,7 +59,7 @@
 		});
 	}
 
-	function handleSetCells(lines, ùm, ùc){
+	function handleSetCells(lines, ùc){
 		$('<button>').text("Définir les couleurs des cases dans Chrall").prependTo(ùc).click(function(){
 			var $ok = $('<div><i>Cases ajoutées</i></div>').hide();
 			$(this).replaceWith($ok);
