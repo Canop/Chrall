7347f4df4ed0fcde4e99e8ac86ce6b9a9bf9a098
Frédéric Donckels <fdonckels@temenos.com>
2018-10-03 16:00:27 +0200

    Fix white-space for lint-check

--- chrome/chrall/chrall_mission.js
+++ chrome/chrall/chrall_mission.js
@@ -1,9 +1,9 @@
 "use strict";
-(function(chrall) {
+(function(chrall){
 
-	chrall.addTroogleLinks = function() {
+	chrall.addTroogleLinks = function(){
 		var $mission_lines = $("[name='ActionForm'] tr");
-		$mission_lines.each(function() {
+		$mission_lines.each(function(){
 			var stepText = this.childNodes[1].textContent;
 			if (0 < stepText.indexOf("monstre")) {
 				addTroogleForMonster(this, stepText);
@@ -17,7 +17,7 @@
 	};
 
 	// Add a link to Troogle for the chosen text, including the current troll's current position
-	function addTroogleLink(row, mainSearch) {
+	function addTroogleLink(row, mainSearch){
 		var url = "http://troogle.iktomi.eu/entities/?entity_search[search]=" + mainSearch + playerPositionParameters();
 		var $link = $("<a/>", {'class': 'gogo', href: url, target: 'troogle', html: "Troogle"});
 		var actionNode = row.childNodes[2];
@@ -25,13 +25,13 @@
 	}
 
 	// Search parameters for the current troll's position
-	function playerPositionParameters() {
+	function playerPositionParameters(){
 		return "&entity_search[position_x]=" + chrall.player().x
 				+ "&entity_search[position_y]=" + chrall.player().y
 				+ "&entity_search[position_z]=" + chrall.player().z;
 	}
 
-	function addTroogleForMonster(row, text) {
+	function addTroogleForMonster(row, text){
 		var mainSearch = "@monstre ";
 		var raceExtract = /de la race des "(.*?)"/i;
 		var match = raceExtract.exec(text);
@@ -58,7 +58,7 @@
 		addTroogleLink(row, mainSearch);
 	}
 
-	function removeAccent(text) {
+	function removeAccent(text){
 		var accent = [
 			/[\300-\306]/g, /[\340-\346]/g, // A, a
 			/[\310-\313]/g, /[\350-\353]/g, // E, e
@@ -79,7 +79,7 @@
 	}
 
 
-	function addTroogleForMushroom(row, text) {
+	function addTroogleForMushroom(row, text){
 		var mainSearch = "@tresor:champignon";
 		addTroogleLink(row, mainSearch);
 	}
