e6b713bdca250f06bbbd49586fa1d95e37e863b7
Frédéric Donckels <fdonckels@temenos.com>
2020-05-06 10:24:25 +0200

    Fix for gowap orders (didn't extract the gowap id correctly anymore)

--- chrome/chrall/chrall_suivants.js
+++ chrome/chrall/chrall_suivants.js
@@ -7,21 +7,18 @@
 		var tableOrdre = $('.mh_tdborder_fo');
 		var lignes = tableOrdre.find('tr');
 		var str = $(lignes[0]).text();
-		var firstDot = str.indexOf('.');
-		var numGowap = parseInt(str.substring(firstDot - 10, firstDot));
-		var position = str.lastIndexOf('X =');
-		var positionText = str.substring(position);
-		var posToken = positionText.split('| ');
-		var gowap_x = parseInt(posToken[0].substring(3).trim());
-		var gowap_y = parseInt(posToken[1].substring(3).trim());
-		var gowap_z = parseInt(posToken[2].substring(3).trim());
+		var match = /(\d+)\. .+?\[.*\s+\d+ PA - X = (-?\d+) \| Y = (-?\d+) \| N = (-?\d+)/.exec(str)
+		var numGowap = parseInt(match[1]);
+		var gowap_x = parseInt(match[2]);
+		var gowap_y = parseInt(match[3]);
+		var gowap_z = parseInt(match[4]);
 		var chemin = new chrall.Chemin(new chrall.Point(gowap_x, gowap_y));
 		var chGowap = new chrall.ChGowap(gowap_x, gowap_y, gowap_z, numGowap);
 		var numMaxOrdre = 0;
 
 		$('<td width=150px></td>')
-		.append('<a name=zoom class=gogo x=' + gowap_x + ' y=' + gowap_y + ' z=' + gowap_z +'>Montrer les alentours</a>')
-		.appendTo($(lignes[0]));
+				.append('<a name=zoom class=gogo x=' + gowap_x + ' y=' + gowap_y + ' z=' + gowap_z + '>Montrer les alentours</a>')
+				.appendTo($(lignes[0]));
 		var movementRegexp = /([0-9\-]+) \| Y=([0-9\-]+) \| N=([0-9\-]+)/;
 		for (var i = 2; i < lignes.length; i++) {
 			var cells = $(lignes[i]).find('td');
@@ -47,7 +44,7 @@
 		var canvas = $("<canvas/>", {id: "cartehall", style: "width:400px;height:400px;"});
 		var carteDiv = $("<div/>", {id: "tdbCarte", style: "text-align: center;"}).text("Carte du Hall");
 		carteDiv.append(
-			$("<span/>", {id: "messageCarte"}).text("Cliquez sur la carte pour donner un ordre de mouvement.")
+				$("<span/>", {id: "messageCarte"}).text("Cliquez sur la carte pour donner un ordre de mouvement.")
 		);
 
 		var form = $('form[name="form1"]');
@@ -62,8 +59,8 @@
 		var player = chrall.player();
 		if (player) {
 			ch.add(new chrall.ChTroll(
-				player.x, player.y, player.z, "blue", player.name,
-				player.sight ? (player.sight.diceNumber + player.sight.physicalBonus) : 0
+					player.x, player.y, player.z, "blue", player.name,
+					player.sight ? (player.sight.diceNumber + player.sight.physicalBonus) : 0
 			));
 		}
 		ch.add(chGowap);
@@ -98,7 +95,7 @@
 
 		// Ordres supplémentaires si le gowap peut s'ébrouer
 		if (0 < $("[name='ai_IdOrdre'] > [value='7']").length) {
-			addMovementNode('S\'ébrouer sur ma case', 'snort', toOwner, player);
+			addMovementNode('S\'ébrouer sur ma case', 'snort-self', toOwner, player);
 			addMovementNode('S\'ébrouer a ses pieds', 'snort', toSelf, chGowap);
 			if (possibleDestination) {
 				addMovementNode('S\'ébrouer aux pieds de : ' + text, 'snort', toDestination, possibleDestination);
@@ -141,7 +138,21 @@
 
 	chrall.triggerGowapAction = function(x, y, z, order, numGowap, numMaxOrdre){
 		return (function(){
-			var movementType = 'move' == order ? '1' : '7'; // move or snort
+			let movementType;
+			switch (order) {
+				case 'move':
+					movementType = '1';
+					break;
+				case 'snort':
+					movementType = '7';
+					break;
+				case 'snort-self':
+					movementType = '12';
+					break;
+				default:
+					movementType = '1';
+					break;
+			} // move or snort
 			window.frameElement.parentNode.children[1].src = '/mountyhall/MH_Follower/FO_NewOrder.php?ai_IdOrdre=' +
 					movementType + "&ai_IdFollower=" + numGowap + "&ai_NbOrdres=" + numMaxOrdre +
 					"&as_Action=Enregistrer+un+nouvel+Ordre;";
